0594c419d46b22a37b67495a5a04d2d888a386d3
sharma1910 <yashraj.sharma@bacancy.us>
2026-01-22 17:48:31 +0530

    correct uploads and answers

--- Day4/assignment/exercise_3.js
+++ Day4/assignment/exercise_3.js
@@ -17,7 +17,6 @@ console.log(myPhone.brand);    // Generic
 //here when we create a myPhone object then it's constructor return an object and in this case a new object which refre to this is hide or overwride by return object so any object which you will create from SmartPhone it will always return {brand: "Generic", os: "Android"}.so brand value is "Generic".
 
 console.log(myPhone.getBrand); // undefined
-//because of a it's not a object of SmartPhone we can't access a SmartPhone.prototype.getBrand() because it;'s created with smartphone because of that it will print "undefined"
-
+//because of a it's not a object of SmartPhone we can't access a SmartPhone.prototype.getBrand() and that's why it will print "undefined".
 
 
--- Day4/extraTask/exercise.js
+++ Day4/extraTask/exercise.js
@@ -9,6 +9,57 @@ const user = {
 
 const user2={...user,name:"yashraj"}
 console.log(user.name, user2.name)
-//
 user2.address.city="ahmedabad"
 console.log(user.address.city,user2.address.city)
+
+/************************************************************************************************* */
+const config = {
+  ui: {
+    theme: "light"
+  }
+};
+
+const copy = { ...config };
+copy.ui.theme = "dark";
+
+console.log(config.ui.theme); // dark
+//here we create a shallow copy of config as a copy and we assigned a theme:"dark" to the copy object but due to ui is non-primitive data type ui of copy and config reffer to the same memory refference so here theme of both object is "dark".
+
+/********************************************************************************************************** */
+function Test() {
+  this.x = 10;
+  return 20;
+}
+
+const t = new Test();
+console.log(t.x); // 10
+//it prints "10" because in constructor return object overrides  but return primitive ignored
+
+
+/************************************************************************************ */
+const base = {
+  items: []
+};
+
+const obj1 = Object.create(base);
+const obj2 = Object.create(base);
+
+obj1.items.push(1);
+
+console.log(obj2.items); // 
+//here because of same prototype array is shared between both object.
+
+
+/******************************************************************************************* */
+
+
+function test(...nums) {
+  nums.push(4);
+  console.log(nums.length);
+}
+
+test(1, 2, 3);
+
+/************************************************************************************************* */
+
+
